b5f7cd93f11faca05c0fb38135a597454baafaf6
Roman Pavlov <roman.pavlov@sharekey.com>
2018-09-30 15:48:33 +0300

    Code solution.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let dividersOfBase = dividers(base);
+
+  let res = number;
+
+  for (key of Object.keys(dividersOfBase)) {
+    const divider = Number(key);
+    let tNumber = number;
+    let tRes = 0;
+    while (tNumber >= divider) {
+      tNumber = Math.floor(tNumber / divider);
+      tRes += tNumber;
+    }
+    tRes = Math.floor(tRes / dividersOfBase[key]);
+    if (tRes === 0) {
+      return res;
+    }
+    if (tRes < res) {
+      res = tRes;
+    }
+  }
+  return res;
+}
+
+function dividers(number) {
+  let divider = 2;
+  let dividers = {};
+  while (number != 1) {
+    if (number % divider === 0) {
+      dividers[divider] = dividers[divider] ? dividers[divider] + 1 : 1;
+      number /= divider;
+    }
+    else {
+      divider++; 
+    }
+  }
+  return dividers;
 }
